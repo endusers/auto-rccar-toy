08c909c000546c906e825540bcac4140a7bb1211
MotoyukiEndo <m.endo.desu@gmail.com>
2025-09-21 22:09:05 +0900

    Modify : GNSS関連TF配信元変更
    GNSS関連のTFをInitialOdometryで配信するように変更

--- initial_odometry/include/initial_odometry/initial_odometry.hpp
+++ initial_odometry/include/initial_odometry/initial_odometry.hpp
@@ -4,14 +4,16 @@
  * @brief       initial_odometry
  * @note        なし
  * 
- * @version     1.2.0
- * @date        2025/06/15
+ * @version     1.3.0
+ * @date        2025/09/21
  * 
  * @copyright   (C) 2023-2025 Motoyuki Endo
  */
 #include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/static_transform_broadcaster.h>
 #include <nav_msgs/msg/odometry.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
 
 class InitialOdometry : public rclcpp::Node
 {
@@ -23,19 +25,29 @@ class InitialOdometry : public rclcpp::Node
 		rclcpp::Clock rosclock_;
 		rclcpp::Time timeInvalid_;
 
-		std::vector<double> initialPose_;
 		std::string frame_id_;
 		std::string child_frame_id_;
+		std::vector<double> initial_pose_;
+		bool publish_tf_;
+
+		bool enable_parent_frame_;
+		std::string parent_frame_;
+		std::vector<double> parent_initial_pose_;
+
+		std::unique_ptr<tf2_ros::StaticTransformBroadcaster> static_tf_br_;
+		std::unique_ptr<tf2_ros::StaticTransformBroadcaster> parent_static_tf_br_;
 
 		rclcpp::TimerBase::SharedPtr timer_;
 
 		rclcpp::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr parameterSubscription_;
+
 		rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_;
 		rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
 
 		void MainLoop( void );
 		void MainCycle( void );
 		void PublishInitialOdom( void );
+		void PublishParentStaticTf( void );
 		void SubscribeGpsOdom( const nav_msgs::msg::Odometry::SharedPtr msg );
 		void UpdateParameters( const rcl_interfaces::msg::ParameterEvent::SharedPtr event );
 
--- initial_odometry/src/initial_odometry.cpp
+++ initial_odometry/src/initial_odometry.cpp
@@ -4,8 +4,8 @@
  * @brief       initial_odometry
  * @note        なし
  * 
- * @version     1.2.0
- * @date        2025/06/15
+ * @version     1.3.0
+ * @date        2025/09/21
  * 
  * @copyright   (C) 2023-2025 Motoyuki Endo
  */
@@ -28,9 +28,18 @@ InitialOdometry::InitialOdometry()
 	timeInvalid_ = rosclock_.now();
 
 	std::vector<double> default_pose = { 0.0, 0.0, 0.0 };
-	initialPose_ = this->declare_parameter( "initial_pose", default_pose );
+
 	frame_id_ = this->declare_parameter<std::string>( "frame_id", "map" );
 	child_frame_id_ = this->declare_parameter<std::string>( "child_frame_id", "odom" );
+	initial_pose_ = this->declare_parameter( "initial_pose", default_pose );
+	publish_tf_ = this->declare_parameter<bool>( "publish_tf", false );
+
+	enable_parent_frame_ = this->declare_parameter<bool>( "enable_parent_frame", false );
+	parent_frame_ = this->declare_parameter<std::string>( "parent_frame", "base_link" );
+	parent_initial_pose_ = this->declare_parameter( "parent_initial_pose", default_pose );
+
+	static_tf_br_ = std::make_unique<tf2_ros::StaticTransformBroadcaster>( this );
+	parent_static_tf_br_ = std::make_unique<tf2_ros::StaticTransformBroadcaster>( this );
 
 	parameterSubscription_ = this->create_subscription<rcl_interfaces::msg::ParameterEvent>(
 		"/parameter_events", 10, std::bind( &InitialOdometry::UpdateParameters, this, _1 ) );
@@ -41,6 +50,8 @@ InitialOdometry::InitialOdometry()
 		"odometry/gps", 10, std::bind( &InitialOdometry::SubscribeGpsOdom, this, _1 ) );
 
 	timer_ = this->create_wall_timer( 100ms, std::bind( &InitialOdometry::MainCycle, this ) );
+
+	PublishParentStaticTf();
 }
 
 InitialOdometry::~InitialOdometry()
@@ -65,7 +76,7 @@ void InitialOdometry::MainCycle( void )
 	}
 }
 
-void InitialOdometry::PublishInitialOdom(void)
+void InitialOdometry::PublishInitialOdom( void )
 {
 	auto odom = nav_msgs::msg::Odometry();
 	double initial_pose_x;
@@ -76,11 +87,11 @@ void InitialOdometry::PublishInitialOdom(void)
 	initial_pose_y = 0.0;
 	initial_pose_a = 0.0;
 
-	if( initialPose_.size() == 3 )
+	if( initial_pose_.size() == 3 )
 	{
-		initial_pose_x = initialPose_[0];
-		initial_pose_y = initialPose_[1];
-		initial_pose_a = initialPose_[2] * ( M_PI / 180 );
+		initial_pose_x = initial_pose_[0];
+		initial_pose_y = initial_pose_[1];
+		initial_pose_a = initial_pose_[2] * ( M_PI / 180 );
 	}
 
 	odom.header.stamp = rosclock_.now();
@@ -95,7 +106,61 @@ void InitialOdometry::PublishInitialOdom(void)
 	quat.setRPY( 0.0, 0.0, initial_pose_a );
 	odom.pose.pose.orientation = tf2::toMsg( quat );
 
+	if( publish_tf_ )
+	{
+		geometry_msgs::msg::TransformStamped tf_msg;
+
+		tf_msg.transform.translation.x = odom.pose.pose.position.x;
+		tf_msg.transform.translation.y = odom.pose.pose.position.y;
+		tf_msg.transform.translation.z = odom.pose.pose.position.z;
+		tf_msg.transform.rotation = odom.pose.pose.orientation;
+
+		tf_msg.header.frame_id = frame_id_;
+		tf_msg.child_frame_id = child_frame_id_;
+		tf_msg.header.stamp = rosclock_.now();
+
+		static_tf_br_->sendTransform( tf_msg );
+	}
 	publisher_->publish( odom );
+}
+
+void InitialOdometry::PublishParentStaticTf( void )
+{
+	if( !enable_parent_frame_ )
+	{
+		return;
+	}
+
+	double initial_pose_x;
+	double initial_pose_y;
+	double initial_pose_a;
+
+	initial_pose_x = 0.0;
+	initial_pose_y = 0.0;
+	initial_pose_a = 0.0;
+
+	if( parent_initial_pose_.size() == 3 )
+	{
+		initial_pose_x = parent_initial_pose_[0];
+		initial_pose_y = parent_initial_pose_[1];
+		initial_pose_a = parent_initial_pose_[2] * ( M_PI / 180 );
+	}
+
+	geometry_msgs::msg::TransformStamped tf_msg;
+
+	tf_msg.header.stamp = rosclock_.now();;
+	tf_msg.header.frame_id = parent_frame_;
+	tf_msg.child_frame_id = frame_id_;
+
+	tf_msg.transform.translation.x = initial_pose_x;
+	tf_msg.transform.translation.y = initial_pose_y;
+	tf_msg.transform.translation.z = 0.0;
+
+	tf2::Quaternion quat;
+	quat.setRPY( 0.0, 0.0, initial_pose_a );
+	tf_msg.transform.rotation = tf2::toMsg( quat );
+
+	parent_static_tf_br_->sendTransform( tf_msg );
 }
 
 void InitialOdometry::SubscribeGpsOdom( const nav_msgs::msg::Odometry::SharedPtr msg )
@@ -110,8 +175,19 @@ void InitialOdometry::UpdateParameters( const rcl_interfaces::msg::ParameterEven
 {
 	if( event->node == this->get_fully_qualified_name() )
 	{
-		this->get_parameter( "initial_pose", initialPose_ );
 		this->get_parameter( "frame_id", frame_id_ );
 		this->get_parameter( "child_frame_id", child_frame_id_ );
+		this->get_parameter( "initial_pose", initial_pose_ );
+		this->get_parameter( "enable_parent_frame", enable_parent_frame_ );
+		this->get_parameter( "parent_frame", parent_frame_ );
+		this->get_parameter( "parent_initial_pose", parent_initial_pose_ );
+	}
+
+	for( const auto &param : event->changed_parameters )
+	{
+		if( param.name == "parent_initial_pose" )
+		{
+			PublishParentStaticTf();
+		}
 	}
 }
